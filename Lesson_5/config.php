795614ee5d3f31a51306560a8f9296b7cd738f0e
Fedor <fesander@mail.ru>
2019-03-15 08:03:28 +0300

    HW_5

---
+++ Lesson_5/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Федор
+ * Date: 13.03.2019
+ * Time: 22:05
+ */
+$server = "localhost";
+$login = "root";
+$pass = "";
+$db = "gallery";
+
+$connect = mysqli_connect($server, $login, $pass, $db);
